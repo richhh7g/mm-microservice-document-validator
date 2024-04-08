fdc23f30a4b9f59a20a0510238bd63d93e4b08db
richhh7g <richhh7g@protonmail.com>
2024-04-08 18:18:00 -0300

    feat: add check document endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DocumentValidationController } from 'src/api/controller';
+import { CNPJWebServiceClientImpl } from 'src/datasource/client';
+import { DocumentValidateUseCaseImpl } from 'src/usecase/document';
+import { CNPJHTTPDataSourceImpl } from 'src/datasource/cnpj';
 
 @Module({
   imports: [
@@ -7,6 +11,21 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath: '.env',
     }),
+  ],
+  controllers: [DocumentValidationController],
+  providers: [
+    {
+      provide: 'DocumentValidateUseCase',
+      useClass: DocumentValidateUseCaseImpl,
+    },
+    {
+      provide: 'CNPJHTTPDataSource',
+      useClass: CNPJHTTPDataSourceImpl,
+    },
+    {
+      provide: 'CNPJWebServiceClient',
+      useClass: CNPJWebServiceClientImpl,
+    },
   ],
 })
 export class AppModule {}
