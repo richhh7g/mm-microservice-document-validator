197cd53a6d6fa407fd57a09f0b0e6e62232d919f
richhh7g <richhh7g@protonmail.com>
2024-04-08 12:57:12 -0300

    chore: add environment config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
 })
 export class AppModule {}
