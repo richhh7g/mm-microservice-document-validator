ed387d448dd8086355cb54939cae249adb10af1f
richhh7g <richhh7g@protonmail.com>
2024-04-08 18:12:53 -0300

    chore: add cnpj datasource

---
+++ src/datasource/cnpj/cnpj.http.datasource.ts
@@ -0,0 +1,34 @@
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { CNPJWebServiceClient } from 'src/datasource/client';
+import { CNPJResponseDTO } from './cnpj.dto';
+
+export interface CNPJHTTPDataSource {
+  findCNPJ(cnpj: string): Promise<CNPJResponseDTO>;
+}
+
+@Injectable()
+export class CNPJHTTPDataSourceImpl implements CNPJHTTPDataSource {
+  constructor(
+    @Inject('CNPJWebServiceClient')
+    private readonly client: CNPJWebServiceClient, // Rate limit of 3 requests per minute
+  ) {}
+
+  async findCNPJ(cnpj: string): Promise<CNPJResponseDTO> {
+    try {
+      const data = await this.client.request({
+        url: `/${cnpj}`,
+        method: 'GET',
+      });
+
+      return {
+        cnpj: data.razao_social,
+        companyName: data.razao_social,
+        currentStatus: data.estabelecimento.situacao_cadastral === 'Ativa',
+      };
+    } catch (err) {
+      Logger.error(err);
+
+      throw err;
+    }
+  }
+}
