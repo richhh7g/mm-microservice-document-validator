1fc0871d6ca29d3d26e902aae50af1751db95e96
richhh7g <richhh7g@protonmail.com>
2024-04-09 06:45:05 -0300

    docs: add main description

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,21 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder().build();
+  const config = new DocumentBuilder()
+    .setTitle('MM Document Microservice')
+    .setVersion('1.0.0')
+    .setLicense(
+      'MIT',
+      'https://github.com/richhh7g/microservico-veracidade-notas/blob/main/LICENSE',
+    )
+    .setContact(
+      'Richhh7g',
+      'https://github.com/richhh7g',
+      'richhh7g@protonmail.com',
+    )
+    .addApiKey({ type: 'apiKey', name: 'x-api-key', in: 'header' }, 'API_KEY')
+    .addTag('Documents', 'Operations about documents')
+    .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
