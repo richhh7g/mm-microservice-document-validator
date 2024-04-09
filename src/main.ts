1b546a55abe9925c2935c67a48b09779307aeb90
richhh7g <richhh7g@protonmail.com>
2024-04-09 06:42:56 -0300

    feat: add docs endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder().build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
