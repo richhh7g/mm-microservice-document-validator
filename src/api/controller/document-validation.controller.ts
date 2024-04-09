3909af7dd5181971bea477b70aaff9df20cd5b8b
richhh7g <richhh7g@protonmail.com>
2024-04-09 06:49:04 -0300

    docs: check document endpoint

--- src/api/controller/document-validation.controller.ts
+++ src/api/controller/document-validation.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Inject, Logger, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpStatus,
+  Inject,
+  Logger,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { ApiBody, ApiResponse, ApiTags, ApiSecurity } from '@nestjs/swagger';
+import { Response } from 'express';
 import { ErrorResponse } from 'src/api/response';
 import { DocumentValidateUseCase } from 'src/usecase/document';
 import {
@@ -14,17 +24,39 @@ export class DocumentValidationController {
   ) {}
 
   @Post()
+  @ApiTags('Documents')
+  @ApiBody({
+    description: 'Check if document is valid',
+    type: CheckDocumentBodyDTO,
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: CheckDocumentResponseDTO,
+    description: 'Returns if document is valid or not',
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    type: ErrorResponse,
+    description: 'Error: Unauthorized',
+  })
+  @ApiResponse({
+    status: HttpStatus.INTERNAL_SERVER_ERROR,
+    type: ErrorResponse,
+    description: 'Error: Internal Server Error',
+  })
+  @ApiSecurity('API_KEY', ['x-api-key'])
   async checkDocument(
     @Body() body: CheckDocumentBodyDTO,
+    @Res() res: Response,
   ): Promise<CheckDocumentResponseDTO | ErrorResponse> {
     try {
       const response = await this.usecase.exec(body.cnpj);
 
-      return { isValid: response };
+      res.status(HttpStatus.OK).json({ isValid: response });
     } catch (err) {
       Logger.error(err);
 
-      return { error: [err.message] };
+      return { errors: [err.message] };
     }
   }
 }
--- src/api/controller/validate-document.dto.ts
+++ src/api/controller/validate-document.dto.ts
@@ -1,7 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CheckDocumentBodyDTO {
+  @ApiProperty({
+    example: '10490181000569',
+    description: 'CNPJ to validate',
+    minimum: 14,
+    maximum: 14,
+  })
   cnpj: string;
 }
 
 export class CheckDocumentResponseDTO {
+  @ApiProperty()
   isValid: boolean;
 }
